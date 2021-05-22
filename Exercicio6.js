16092f5ea781670499f76363040a245996075653
jeniferplacido <jenifer.sdti@gmail.com>
2021-05-22 00:13:03 -0300

    Exercicio 6

---
+++ Exercicio6.js
@@ -0,0 +1,25 @@
+// 6 - Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.
+
+let numero = 1
+
+if(numero ==1){
+    console.log("Hoje é Domingo");
+}
+if(numero ==2){
+    console.log("Hoje é Segunda");
+}
+if(numero ==3){
+    console.log("Hoje é Terça");
+}
+if(numero ==4){
+    console.log("Hoje é Quarta");
+}
+if(numero ==5){
+    console.log("Hoje é Quinta");
+}
+if(numero ==6){
+    console.log("Hoje é Sexta");
+}
+if(numero ==7){
+    console.log("Hoje é Sábado");
+}
