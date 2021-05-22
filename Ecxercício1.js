bf4a397f55614a666e564ae4259549c74ae8a8e9
jeniferplacido <jenifer.sdti@gmail.com>
2021-05-21 23:25:04 -0300

    Exercício 1

---
+++ Ecxercício1.js
@@ -0,0 +1,9 @@
+// 1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. 
+// Exiba no console o valor do desconto e o preço a pagar.
+
+let valor = 100
+let desconto = valor * (20/100)
+console.log("O desconto é de: " +desconto )
+
+let valorComDesconto = valor - desconto
+console.log("O valor a pagar com desconto é: " +valorComDesconto)
