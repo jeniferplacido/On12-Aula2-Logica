044f34f2ef056cbd975ac0410d9dc39674eb799a
jeniferplacido <jenifer.sdti@gmail.com>
2021-05-22 00:03:33 -0300

    Exercicio5

---
+++ Exercício5.js
@@ -0,0 +1,9 @@
+//  5 - Receba do usuários 3 números e calcule a média entre eles.
+
+let numero1 = 10
+let numero2 = 5
+let numero3 = 5
+
+let media = (numero1+numero3+numero3) / 3
+
+console.log("A média dos números digitados é: "+media)
