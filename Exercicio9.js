df41289442ac570865799543bb2f5967cb66e1f2
jeniferplacido <jenifer.sdti@gmail.com>
2021-05-22 00:30:52 -0300

    Exercicio 9

---
+++ Exercicio9.js
@@ -0,0 +1,9 @@
+//9 - Crie uma variavel que retorna a palavra (impar/par) de acordo com seu parâmetro.
+
+
+let numero = 9
+    if (numero % 2 == 0) {
+        console.log("O número é par");
+    } else {
+        console.log("O número é ímpar");
+    }
